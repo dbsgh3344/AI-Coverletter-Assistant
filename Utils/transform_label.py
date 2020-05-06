8954ec2f74db24adb9d8ffd4519a09d931b44bdf
Younho Song <dbsgh3344@naver.com>
2020-05-06 18:13:46 +0900

    delete class module
    only use function

--- Utils/transform_label.py
+++ Utils/transform_label.py
@@ -4,46 +4,43 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-class TransformLabel:
-    # def __init__(self):
-        # self.manyhot_labels
 
 
-    def manyhot_label(self,label_col):
-        
-        ''' transform str or int label to manyhot encoding label'''
+def manyhot_label(label_col):
 
-        '''Parameters
-            
-            label_col : Input Series Type str or int label column '''
-        
-        pat = re.compile('[^0-9]')
-        label_col = label_col.apply(lambda x : np.array(x))
-        int_label = label_col[label_col.notnull()].apply(lambda x: [int(i) for i in list(pat.sub('',x))])
+    ''' transform str or int label to manyhot encoding label'''
 
-        mlb = MultiLabelBinarizer()
-        manyhot_labels = mlb.fit_transform(int_label)
+    '''Parameters
 
-        return manyhot_labels
+        label_col : Input Series Type str or int label column '''
 
-    def label_visualize(self,manyhot_labels):
-        lists = [0] * 9 
-        for i in manyhot_labels:
-            for j in range(9):
-                if i[j] ==1:
-                    lists[j] +=1
+    pat = re.compile('[^0-9]')
+    label_col = label_col.apply(lambda x : np.array(x))
+    int_label = label_col[label_col.notnull()].apply(lambda x: [int(i) for i in list(pat.sub('',x))])
 
-        
-        ax= sns.barplot(np.arange(len(manyhot_labels[0])),y=lists)
+    mlb = MultiLabelBinarizer()
+    manyhot_labels = mlb.fit_transform(int_label)
 
-        #adding the text labels
-        rects = ax.patches
-        labels = lists
-        for rect, label in zip(rects, labels):
-            height = rect.get_height()
-            ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom', fontsize=10)
-        
-        return plt.show()
+    return manyhot_labels
+
+def label_visualize(manyhot_labels):
+    lists = [0] * 9 
+    for i in manyhot_labels:
+        for j in range(9):
+            if i[j] ==1:
+                lists[j] +=1
+
+
+    ax= sns.barplot(np.arange(len(manyhot_labels[0])),y=lists)
+
+    #adding the text labels
+    rects = ax.patches
+    labels = lists
+    for rect, label in zip(rects, labels):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2, height + 5, label, ha='center', va='bottom', fontsize=10)
+
+    return plt.show()
         
 
 
